92d682f6cb8c3af6ea20c3e4053158a2c07296d1
dkurchigin <kurchigin.dmitry@yandex.ru>
2023-10-15 15:06:34 +0300

    added first ver of servo lib

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,11 @@ use arduino_hal::port::mode::PwmOutput;
 use arduino_hal::simple_pwm::Timer1Pwm;
 use arduino_hal::port::Pin;
 
-const LEFT_DUTY: u8 = 12;
-const STOP_DUTY: u8 = 22;
-const RIGHT_DUTY: u8 = 32;
+pub const LEFT_DUTY: u8 = 12;
+pub const STOP_DUTY: u8 = 22;
+pub const RIGHT_DUTY: u8 = 32;
 
-struct MG995 {
+pub struct MG995 {
     pin: Pin<PwmOutput<Timer1Pwm>, PB1>,
     left_duty: u8,
     stop_duty: u8,
@@ -15,27 +15,27 @@ struct MG995 {
 }
 
 impl MG995 {
-    fn new(pin: Pin<PwmOutput<Timer1Pwm>, PB1>, left_duty: u8, stop_duty:u8, right_duty: u8) -> MG995 {
+    pub fn new(pin: Pin<PwmOutput<Timer1Pwm>, PB1>, left_duty: u8, stop_duty:u8, right_duty: u8) -> MG995 {
         MG995 { pin, left_duty, stop_duty, right_duty }
     }
 
-    fn enable(&mut self) {
+    pub fn enable(&mut self) {
         self.pin.enable()
     }
 
-    fn disable(&mut self) {
+    pub fn disable(&mut self) {
         self.pin.disable()
     }
 
-    fn stop(&mut self) {
+    pub fn stop(&mut self) {
         self.pin.set_duty(self.stop_duty)
     }
 
-    fn left(&mut self) {
+    pub fn left(&mut self) {
         self.pin.set_duty(self.left_duty)
     }
 
-    fn right(&mut self) {
+    pub fn right(&mut self) {
         self.pin.set_duty(self.right_duty)
     }
 }
