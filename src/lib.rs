1d78ab0e78e6eb2514d58383e18c3a4cd00635c5
dkurchigin <kurchigin.dmitry@yandex.ru>
2023-10-16 17:29:16 +0300

    added first ver of servo lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+//! # MG995 Servo
+//!
+//! A library for MG995 servomotor
+//! Minimal functionality, easy to use
+
 #![no_std]
 use arduino_hal::hal::port::PB1;
 use arduino_hal::port::mode::PwmOutput;
@@ -15,6 +20,27 @@ pub struct MG995 {
     right_duty: u8
 }
 
+/// # Example
+///
+/// ```
+///     let dp = arduino_hal::Peripherals::take().unwrap();
+///     let pins = arduino_hal::pins!(dp);
+///     let timer1 = Timer1Pwm::new(dp.TC1, simple_pwm::Prescaler::Prescale1024);
+///     let servo_pin = pins.d9.into_output().into_pwm(&timer1);
+///
+///     let mut mg995 = MG995::new(servo_pin, STOP_DUTY, LEFT_DUTY, RIGHT_DUTY);
+///     mg995.enable();
+///     loop {
+///         mg995.stop();
+///         arduino_hal::delay_ms(500);
+///         mg995.right();
+///         arduino_hal::delay_ms(500);
+///         mg995.stop();
+///         arduino_hal::delay_ms(500);
+///         mg995.left();
+///         arduino_hal::delay_ms(500);
+///     }
+/// ```
 impl MG995 {
     pub fn new(pin: Pin<PwmOutput<Timer1Pwm>, PB1>, left_duty: u8, stop_duty:u8, right_duty: u8) -> MG995 {
         MG995 { pin, left_duty, stop_duty, right_duty }
