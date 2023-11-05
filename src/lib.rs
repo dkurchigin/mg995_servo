b9dd4420c007c0929dc83e38a5a75fc7e4639a43
dkurchigin <kurchigin.dmitry@yandex.ru>
2023-11-05 17:13:22 +0300

    edited pins and added is_inverted

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@
 //! Minimal functionality, easy to use
 
 #![no_std]
-use arduino_hal::hal::port::PB1;
+use arduino_hal::hal::port::{PB1, PB2, PB3, PD3, PD5, PD6};
 use arduino_hal::port::mode::PwmOutput;
 use arduino_hal::simple_pwm::Timer1Pwm;
 use arduino_hal::port::Pin;
@@ -13,11 +13,21 @@ pub const LEFT_DUTY: u8 = 12;
 pub const STOP_DUTY: u8 = 22;
 pub const RIGHT_DUTY: u8 = 32;
 
+pub enum UnoPinsPWM {
+    PB1(PB1),
+    PB2(PB2),
+    PB3(PB3),
+    PD3(PD3),
+    PD5(PD5),
+    PD6(PD6)
+}
+
 pub struct MG995 {
-    pin: Pin<PwmOutput<Timer1Pwm>, PB1>,
+    pin: Pin<PwmOutput<Timer1Pwm>, UnoPinsPWM>,
     left_duty: u8,
     stop_duty: u8,
-    right_duty: u8
+    right_duty: u8,
+    is_inverted: bool
 }
 
 /// # Example
@@ -28,7 +38,7 @@ pub struct MG995 {
 ///     let timer1 = Timer1Pwm::new(dp.TC1, simple_pwm::Prescaler::Prescale1024);
 ///     let servo_pin = pins.d9.into_output().into_pwm(&timer1);
 ///
-///     let mut mg995 = MG995::new(servo_pin, STOP_DUTY, LEFT_DUTY, RIGHT_DUTY);
+///     let mut mg995 = MG995::new(servo_pin, STOP_DUTY, LEFT_DUTY, RIGHT_DUTY, false);
 ///     mg995.enable();
 ///     loop {
 ///         mg995.stop();
@@ -42,8 +52,8 @@ pub struct MG995 {
 ///     }
 /// ```
 impl MG995 {
-    pub fn new(pin: Pin<PwmOutput<Timer1Pwm>, PB1>, left_duty: u8, stop_duty:u8, right_duty: u8) -> MG995 {
-        MG995 { pin, left_duty, stop_duty, right_duty }
+    pub fn new(pin: Pin<PwmOutput<Timer1Pwm>, UnoPinsPWM>, left_duty: u8, stop_duty:u8, right_duty: u8, is_inverted: bool) -> MG995 {
+        MG995 { pin, left_duty, stop_duty, right_duty, is_inverted }
     }
 
     pub fn enable(&mut self) {
@@ -59,10 +69,18 @@ impl MG995 {
     }
 
     pub fn left(&mut self) {
-        self.pin.set_duty(self.left_duty)
+        if self.is_inverted {
+            self.pin.set_duty(self.right_duty)
+        } else {
+            self.pin.set_duty(self.left_duty)
+        }
     }
 
     pub fn right(&mut self) {
-        self.pin.set_duty(self.right_duty)
+        if self.is_inverted {
+            self.pin.set_duty(self.left_duty)
+        } else {
+            self.pin.set_duty(self.right_duty)
+        }
     }
 }
